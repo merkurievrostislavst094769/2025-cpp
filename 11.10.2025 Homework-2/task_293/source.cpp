638a3b36e116d4b1fd63e7f4dbbfab07cc482572
st094769 <rostislav.merkuriev@gmail.com>
2025-10-18 17:18:14 +0300

    Update source.cpp

--- 11.10.2025 Homework-2/task_293/source.cpp
+++ 11.10.2025 Homework-2/task_293/source.cpp
@@ -1,69 +1,53 @@
 #include <cstdio>
 
 int main() {
-    char startCol = 0;
-    char endCol = 0;
-    int startRow = 0;
-    int endRow = 0;
+    char x1 = 0;
+    char x2 = 0;
+    int y1 = 0;
+    int y2 = 0;
+    int c = 0;
 
-    scanf_s("%c%d %c%d", &startCol, &startRow, &endCol, &endRow);
+    scanf(" %c%d %c%d", &x1, &y1, &x2, &y2);
 
-    int dc = endCol - startCol; 
-    int dr = endRow - startRow;
+    int dx = x2 - x1;
+    if (dx < 0) dx = -dx;
+    int dy = y2 - y1;
+    if (dy < 0) dy = -dy;
 
-    int king = 0;
-    int queen = 0;
-    int rook = 0;
-    int bishop = 0;
-    int knight = 0;
-    int pawn = 0;
-
-    if ((dc >= -1 && dc <= 1) && (dr >= -1 && dr <= 1) && !(dc == 0 && dr == 0))
-    {
-        king = 1;
+    if ((x1 == x2 && y2 == y1 + 1) || (x1 == x2 && y1 == 2 && y2 == 4)) {
+        printf("Pawn ");
+        c = 1;
     }
 
-    if ((dc == 0 && dr != 0) || (dr == 0 && dc != 0) || (dc == dr) || (dc == -dr)) 
-    {
-        queen = 1;
+    if ((dx == 1 && dy == 2) || (dx == 2 && dy == 1)) {
+        printf("Knight ");
+        c = 1;
     }
 
-    if ((dc == 0 && dr != 0) || (dr == 0 && dc != 0)) 
-    {
-        rook = 1;
+    if ((dx <= 1) && (dy <= 1)) {
+        printf("King ");
+        c = 1;
+    }
+    
+    if ((x1 == x2 && y1 != y2) || (y1 == y2 && x1 != x2)) {
+        printf("Rook ");
+        c = 1;
     }
 
-    if ((dc == dr) || (dc == -dr)) 
-    {
-       bishop = 1;
+    if (dx == dy) {
+        printf("Bishop ");
+        c = 1;
     }
 
-    if (((dc == 2 || dc == -2) && (dr == 1 || dr == -1)) || ((dc == 1 || dc == -1) && (dr == 2 || dr == -2))) 
-    {
-        knight = 1;
+    if ((x1 == x2 && y1 != y2) || (y1 == y2 && x1 != x2) || (dx == dy)) {
+        printf("Queen ");
+        c = 1;
     }
 
-    if (dc == 0) 
-    {
-        if (startRow == 2 && (dr == 1 || dr == 2)) {
-            pawn = 1;
-        }
-        else if (startRow != 2 && dr == 1) {
-            pawn = 1;
-        }
+    if (c == 0) {
+        printf("Nobody");
     }
 
-    int n = 1;
-    if (king) { printf("King\n"); n = 0; }
-    if (queen) { printf("Queen\n"); n = 0; }
-    if (rook) { printf("Rook\n"); n = 0; }
-    if (bishop) { printf("Bishop\n"); n = 0; }
-    if (knight) { printf("Knight\n"); n = 0; }
-    if (pawn) { printf("Pawn\n"); n = 0; }
-
-    if (n) {
-        printf("Nobody\n");
-    }
-
+    printf("\n");
     return 0;
 }
